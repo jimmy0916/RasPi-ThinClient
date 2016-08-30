084fdb7a80598687148a6e7a0e6d60c7ce657681
Justin Sinc <jpsinkula@hotmail.com>
2016-08-30 17:59:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ apt-get install libxss-dev gcc
 gcc -I /root/RasPi-ThinClient/ -o getIdle getIdle.c -lXss -lX11
 mv /root/RasPi-ThinClient/getIdle /usr/bin/getIdle
 
-# add the rdp script to /etc/init.d/ and run it on boot
+# move the rdp shell script to /usr/bin
+mv /root/RasPi-ThinClient/rdesktop.sh /usr/bin/rdp
+
+# add the rdp startup script to /etc/init.d/ and run it on boot
 mv /root/RasPi-ThinClient/rdp /etc/init.d/rdp
 chmod 755 /etc/init.d/rdp
 update-rc.d rdp defaults
