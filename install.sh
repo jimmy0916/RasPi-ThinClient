23ed78301a5ba5abfcfa02d349cce6cff80f15ee
Justin Sinc <jpsinkula@hotmail.com>
2016-08-30 17:54:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 # install required dependencies
 apt-get install libxss-dev gcc
 
-# compile the getIdle C script
+# compile the getIdle C script and install it in /usr/bin
 gcc -I /root/RasPi-ThinClient/ -o getIdle getIdle.c -lXss -lX11
+mv /root/RasPi-ThinClient/getIdle /usr/bin/getIdle
 
 # add the rdp script to /etc/init.d/ and run it on boot
 mv /root/RasPi-ThinClient/rdp /etc/init.d/rdp
