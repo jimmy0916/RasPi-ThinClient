3aaa2170dfe671955ceeec7302960d5c82826485
jimmy <jimmy.bih@gmail.com>
2017-03-20 17:43:13 +0800

    Update rdesktop.sh

--- rdesktop.sh
+++ rdesktop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-domain=''
-server='192.168.20.106'
-idleTime=$(getIdle)
+domain='WORKGROUP'
+server='10.10.0.171'
+#idleTime=$(getIdle)
+idleTime=3
 
 while [ $idleTime -lt 300000 ]; do
-	xinit /usr/bin/rdesktop -f -d "$domain" -u "" "$server" -- :1
+	xinit /usr/bin/rdesktop -fPb -u scada -p scada -a 16 -r sound:local -x lan -d "$domain" "$server" -- :1
 done
